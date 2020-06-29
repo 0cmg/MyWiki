68d4241e8cfd00f2345e370a6dab008a1215a6a6
qin.wang <qin.wang@kingstarfintech.com>
2020-06-29 09:02:27 +0800

    提交代码

---
+++ my-redis/src/main/java/com/cmg/redis/springdata/dao/HelloDao.java
@@ -0,0 +1,31 @@
+package com.cmg.redis.springdata.dao;
+
+import com.cmg.redis.springdata.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class HelloDao {
+    @Autowired
+    RedisTemplate redisTemplate;
+    public void set(String key, String value) {
+        ValueOperations ops = redisTemplate.opsForValue();
+        ops.set(key, value);
+    }
+    public String get(String key) {
+        ValueOperations ops = redisTemplate.opsForValue();
+        return ops.get(key).toString();
+    }
+    public void setuser(User user) {
+        ValueOperations ops = redisTemplate.opsForValue();
+        ops.set(user.getId(), user);
+    }
+    public User getuser(String id) {
+        ValueOperations<String, User> ops = redisTemplate.opsForValue();
+        User user = ops.get(id);
+        System.out.println(user);
+        return user;
+    }
+}
