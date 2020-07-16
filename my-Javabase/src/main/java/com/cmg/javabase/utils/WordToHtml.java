b72d0163c31c23eb59be1f84b9d864cfdec5b580
qin.wang <qin.wang@kingstarfintech.com>
2020-07-16 10:09:57 +0800

    提交代码

---
+++ my-Javabase/src/main/java/com/cmg/javabase/utils/WordToHtml.java
@@ -0,0 +1,86 @@
+package com.cmg.javabase.utils;
+
+import org.apache.poi.hwpf.HWPFDocument;
+import org.apache.poi.hwpf.converter.WordToHtmlConverter;
+import org.jsoup.Jsoup;
+import org.w3c.dom.Document;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.*;
+
+
+/**
+ * @Name MyWiki com.cmg.javabase.utils
+ * @Author cmg
+ * @Date 2020/7/15 13:32
+ * @Desciption TODO
+ **/
+public class WordToHtml {
+    public static void main(String argv[]) {
+        try {
+            convert2Html("c:\\file\\开发文档\\上海农商行\\打印\\表单一《上海农商银行人民币贵金属即期交易审批书》.doc","c:\\file\\开发文档\\上海农商行\\打印\\表单一《上海农商银行人民币贵金属即期交易审批书》.html");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    //输出html文件
+    public static void writeFile(String content, String path) {
+        FileOutputStream fos = null;
+        BufferedWriter bw = null;
+        org.jsoup.nodes.Document doc = Jsoup.parse(content);
+        content=doc.html();
+        try {
+            File file = new File(path);
+            fos = new FileOutputStream(file);
+            bw = new BufferedWriter(new OutputStreamWriter(fos,"UTF-8"));
+            bw.write(content);
+        } catch (FileNotFoundException fnfe) {
+            fnfe.printStackTrace();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        } finally {
+            try {
+                if (bw != null)
+                    bw.close();
+                if (fos != null)
+                    fos.close();
+            } catch (IOException ie) {
+            }
+        }
+    }
+
+    //word 转 html
+    public static void convert2Html(String fileName, String outPutFile)
+            throws TransformerException, IOException,
+            ParserConfigurationException {
+
+        HWPFDocument wordDocument = new HWPFDocument(new FileInputStream(fileName));//WordToHtmlUtils.loadDoc(new FileInputStream(inputFile));
+        //兼容2007 以上版本
+        WordToHtmlConverter wordToHtmlConverter = new WordToHtmlConverter(
+                DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument());
+        wordToHtmlConverter.processDocument(wordDocument);
+        Document htmlDocument = wordToHtmlConverter.getDocument();
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        DOMSource domSource = new DOMSource(htmlDocument);
+        StreamResult streamResult = new StreamResult(out);
+
+
+        TransformerFactory tf = TransformerFactory.newInstance();
+        Transformer serializer = tf.newTransformer();
+        serializer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+        serializer.setOutputProperty(OutputKeys.INDENT, "yes");
+        serializer.setOutputProperty(OutputKeys.METHOD, "HTML");
+        serializer.transform(domSource, streamResult);
+        out.close();
+        writeFile(new String(out.toByteArray()), outPutFile);
+    }
+}
