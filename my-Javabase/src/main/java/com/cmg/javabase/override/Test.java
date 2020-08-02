bab4d3411e869a9e2e23cffea60e62c3d94499ac
cmg <copgwq@163.com>
2020-08-02 08:24:57 +0800

    代码提交

---
+++ my-Javabase/src/main/java/com/cmg/javabase/override/Test.java
@@ -0,0 +1,16 @@
+package com.cmg.javabase.override;
+
+/**
+ * @Name MyWiki com.cmg.javabase.override
+ * @Author cmg
+ * @date 2020/8/2 8:07
+ * @Description TODO
+ **/
+public class Test {
+    private void test(){
+        System.out.println("父类的private方法！");
+    }
+    public void test2(){
+        System.out.println("父类的publi方法！");
+    }
+}
---
+++ my-Javabase/src/main/java/com/cmg/javabase/override/Test1.java
@@ -0,0 +1,14 @@
+package com.cmg.javabase.override;
+
+/**
+ * @Name MyWiki com.cmg.javabase.override
+ * @Author cmg
+ * @date 2020/8/2 8:08
+ * @Description TODO
+ **/
+public class Test1 extends Test{
+    @Override
+    public void test2() {
+        super.test2();
+    }
+}
