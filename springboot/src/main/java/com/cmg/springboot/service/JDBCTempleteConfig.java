e1ced752eae9b6b9d7298b0ac71a0aa92f424c61
cmg <copgwq@163.com>
2020-08-09 20:13:11 +0800

    代码提交

---
+++ springboot/src/main/java/com/cmg/springboot/service/JDBCTempleteConfig.java
@@ -0,0 +1,14 @@
+package com.cmg.springboot.service;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Name MyWiki com.cmg.springboot.service
+ * @Author cmg
+ * @date 2020/8/9 16:20
+ * @Description TODO
+ **/
+@Configuration
+public class JDBCTempleteConfig {
+
+}
