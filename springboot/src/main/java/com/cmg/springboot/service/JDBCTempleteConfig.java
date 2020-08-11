eccdb5fbdc8635dc79a0fbdca6193cb790734cce
cmg <copgwq@163.com>
2020-08-11 20:36:36 +0800

    代码提交

--- springboot/src/main/java/com/cmg/springboot/service/JDBCTempleteConfig.java
+++ springboot/src/main/java/com/cmg/springboot/service/JDBCTempleteConfig.java
@@ -1,6 +1,13 @@
 package com.cmg.springboot.service;
 
+import com.alibaba.druid.pool.DruidDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
 
 /**
  * @Name MyWiki com.cmg.springboot.service
@@ -9,6 +16,34 @@ import org.springframework.context.annotation.Configuration;
  * @Description TODO
  **/
 @Configuration
+@PropertySource(value = "classpath:db.properties")
 public class JDBCTempleteConfig {
+    @Value("${jdbcDriverClassName}")
+    String jdbcDriverClassName;
 
+    @Value("${jdbcUrl}")
+    String jdbcUrl;
+
+    @Value("${jdbcUserName}")
+    String jdbcUserName;
+
+    @Value("${jdbcPassWord}")
+    String jdbcPassWord;
+
+    @Bean
+    public DataSource dataSource(){
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setDriverClassName(jdbcDriverClassName);
+        dataSource.setUrl(jdbcUrl);
+        dataSource.setUsername(jdbcUserName);
+        dataSource.setPassword(jdbcPassWord);
+        return dataSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource){
+        JdbcTemplate jdbcTemplate = new JdbcTemplate();
+        jdbcTemplate.setDataSource(dataSource);
+        return jdbcTemplate;
+    }
 }
--- springboot/src/test/java/com/cmg/springboot/SpringbootApplicationTests.java
+++ springboot/src/test/java/com/cmg/springboot/SpringbootApplicationTests.java
@@ -1,13 +1,66 @@
 package com.cmg.springboot;
 
+import com.cmg.springboot.entity.User;
+import com.cmg.springboot.service.JDBCTempleteConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.*;
+import java.util.List;
 
 @SpringBootTest
 class SpringbootApplicationTests {
+    @Test
+    public void test(){
+        AnnotationConfigApplicationContext annotationConfigApplicationContext = new AnnotationConfigApplicationContext(JDBCTempleteConfig.class);
+        JdbcTemplate jdbcTemplate = annotationConfigApplicationContext.getBean(JdbcTemplate.class);
+        List<User> list = jdbcTemplate.query("select * from user", new RowMapper<User>() {
+
+            @Override
+            public User mapRow(ResultSet rs, int i) throws SQLException {
+                User user = new User();
+                user.setId(rs.getInt("id"));;
+                user.setName(rs.getString("name"));
+                user.setSex(rs.getString("sex"));
+                user.setAddr(rs.getString("addr"));
+                return user;
+            }
+        });
+        System.out.println(list);
+    }
 
     @Test
-    void contextLoads() {
+    public void test2() throws Exception {
+        Connection conn = null;
+        Class.forName("com.mysql.jdbc.Driver");
+        conn = DriverManager.getConnection("jdbc.mysql://localhost:3306/sprintboot?characterEncoding=UTF-8&useUnicode=true&useSSL=true&serverTimezone=UTC",
+                "root","123456");
+
+        Statement stmt = conn.createStatement();
+        String sql;
+        sql = "SELECT * FROM user";
+        ResultSet rs = stmt.executeQuery(sql);
+        while(rs.next()){
+            // 通过字段检索
+            int id  = rs.getInt("id");
+            String name = rs.getString("name");
+            String sex = rs.getString("sex");
+            String addr = rs.getString("addr");
+
+            // 输出数据
+            System.out.print("ID: " + id);
+            System.out.print(", 名字: " + name);
+            System.out.print(", 性别: " + sex);
+            System.out.print(", 地址: " + addr);
+            System.out.print("\n");
+        }
+        // 完成后关闭
+        rs.close();
+        stmt.close();
+        conn.close();
     }
 
 }
