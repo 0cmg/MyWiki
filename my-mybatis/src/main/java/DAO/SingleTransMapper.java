f6b9ad8267532d0832c09e70886b733987d87a11
qin.wang <qin.wang@kingstarfintech.com>
2020-09-21 16:34:11 +0800

    提交代码： 四种mybatis传参方式

--- my-mybatis/src/main/java/DAO/SingleTransMapper.java
+++ my-mybatis/src/main/java/DAO/SingleTransMapper.java
@@ -1,7 +1,14 @@
 package DAO;
 
+import DTO.SingleCondition;
 import DTO.SingletransDTO;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.HashMap;
 
 public interface SingleTransMapper {
     SingletransDTO selectSingle(String transNo);
+    SingletransDTO selectSingleByClass(SingleCondition singleCondition);
+    SingletransDTO selectSingleByMultCondition(HashMap<String,Object> param);
+    SingletransDTO selectSingleByParam(@Param("param1")String transNo, @Param("param2") String commandCode);
 }
--- my-mybatis/src/main/java/controller/TestController.java
+++ my-mybatis/src/main/java/controller/TestController.java
@@ -1,6 +1,7 @@
 package controller;
 
 import DAO.SingleTransMapper;
+import DTO.SingleCondition;
 import DTO.SingletransDTO;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
@@ -9,6 +10,7 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
 
 public class TestController {
     public static void main(String[] args) throws IOException {
@@ -18,11 +20,11 @@ public class TestController {
         //创建session
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
         try (SqlSession session = sqlSessionFactory.openSession()) {
-            //获取单笔映射对象
-            SingleTransMapper mapper = session.getMapper(SingleTransMapper.class);
-            //根据流水号查询
-            SingletransDTO blog = mapper.selectSingle("642EHDCS899XKF8P");
 
+//            SingletransDTO blog = selectSingle(session);
+//            SingletransDTO blog = selectSingleByClass(session);
+//            SingletransDTO blog = selectSingleByMultCondition(session);
+            SingletransDTO blog = selectSingleByParam(session);
             if(blog != null) {
                 System.out.println(blog);
             }else{
@@ -32,5 +34,36 @@ public class TestController {
         }catch (Exception exception){
             System.out.println(exception.getMessage());
         }
+    }
+
+    public static SingletransDTO selectSingle(SqlSession session){
+        //获取单笔映射对象
+        SingleTransMapper mapper = session.getMapper(SingleTransMapper.class);
+        //根据流水号查询
+        SingletransDTO blog = mapper.selectSingle("642EHDCS899XKF8P");
+        return blog;
+    }
+
+    public static SingletransDTO selectSingleByClass(SqlSession session){
+        SingleTransMapper mapper = session.getMapper(SingleTransMapper.class);
+        SingletransDTO blog = mapper.selectSingleByClass(new SingleCondition( "642EHDCS899XKF8P","123"));
+        return blog;
+    }
+    public static SingletransDTO selectSingleByMultCondition(SqlSession session){
+        //获取单笔映射对象
+        SingleTransMapper mapper = session.getMapper(SingleTransMapper.class);
+        //根据流水号查询
+        HashMap<String,Object> param = new HashMap<String,Object>();
+        param.put("transNo","642EHDCS899XKF8P");
+        param.put("commandCode","123");
+        SingletransDTO blog = mapper.selectSingleByMultCondition(param);
+        return blog;
+    }
+
+    public static SingletransDTO selectSingleByParam(SqlSession session){
+        //获取单笔映射对象
+        SingleTransMapper mapper = session.getMapper(SingleTransMapper.class);
+        SingletransDTO blog = mapper.selectSingleByParam("642EHDCS899XKF8P","123");
+        return blog;
     }
 }
