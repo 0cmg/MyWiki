60bea18555c1417f210290a67cddc60de3105eb4
qin.wang <qin.wang@kingstarfintech.com>
2021-04-06 17:32:17 +0800

    提交

--- my-Javabase/src/main/java/com/cmg/javabase/offer/ListNode.java
+++ my-Javabase/src/main/java/com/cmg/javabase/offer/ListNode.java
@@ -23,4 +23,12 @@ public class ListNode {
     public void setNext(ListNode next) {
         this.next = next;
     }
+
+    @Override
+    public String toString() {
+        return "ListNode{" +
+                "val=" + val +
+                ", next=" + next +
+                '}';
+    }
 }
